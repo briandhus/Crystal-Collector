955ed7f1f7874a68fa700cbdaf584571e1fb8ba0
Revinate <revinate@calvisitor-10-105-169-4.calvisitor.1918.berkeley.edu>
2017-06-19 16:35:29 -0700

    added wins, losses tracker

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,64 @@
-var score = 0;
-var wins = 0;
-var losses = 0;
+$(document).ready(function(){
 
-// computer generates random number between 19 + 120.
+	var score = 0;
+	var wins = 0;
+	var losses = 0;
 
-var computerNumber = Math.floor((Math.random() * 102) + 19);
+	// computer generates random number between 19 + 120.
 
-$('#targetScore').text(computerNumber);
+	var computerNumber = Math.floor((Math.random() * 102) + 19);
 
-var reset = function () {
+	$('#targetScore').text(computerNumber);
+
 	
-}
 
-// make 4 chrystals for player to click
-// each crystal is assigned a random value between 1 + 12.
-// player clicks crystals and the assigned number gets added to a total.
-var rubyValue = Math.floor((Math.random() * 12) + 1); 
+	// make 4 crystals for player to click
+	// each crystal is assigned a random value between 1 + 12.
+	// player clicks crystals and the assigned number gets added to a total.
+	var rubyValue = Math.floor((Math.random() * 12) + 1); 
 
-$('#ruby').on('click', function() {
-	 score += rubyValue;
-	$('#totalScore').text(score);
-});
+	$('#ruby').on('click', function() {
+		 score += rubyValue;
+		 winChecker(score)
+		$('#totalScore').text(score);
+	});
 
-var diamondValue = Math.floor((Math.random() * 12) + 1); 
+	var diamondValue = Math.floor((Math.random() * 12) + 1); 
 
-$('#diamond').on('click', function() {
-	 score += diamondValue;
-	$('#totalScore').text(score);
-});
+	$('#diamond').on('click', function() {
+		 score += diamondValue;
+		 winChecker(score)
+		$('#totalScore').text(score);
+	});
 
-var amberValue = Math.floor((Math.random() * 12) + 1); 
+	var amberValue = Math.floor((Math.random() * 12) + 1); 
 
-$('#amber').on('click', function() {
-	 score += amberValue;
-	$('#totalScore').text(score);
-});
+	$('#amber').on('click', function() {
+		 score += amberValue;
+		 winChecker(score)
+		$('#totalScore').text(score);
+	});
 
-var emeraldValue = Math.floor((Math.random() * 12) + 1); 
+	var emeraldValue = Math.floor((Math.random() * 12) + 1); 
 
-$('#emerald').on('click', function() {
-	 score += emeraldValue;
-	$('#totalScore').text(score);
-});
+	$('#emerald').on('click', function() {
+		 score += emeraldValue;
+		 winChecker(score)
+		$('#totalScore').text(score);
+	});
 
-if (score === computerNumber) {
-	wins++;
-} else if (score > computerNumber) {
-	losses++;
-	$('#targetScore').reset();
-	$('totalScore').reset();
-} 
+
+var winChecker = function(score){
+		if (score === computerNumber) {
+			wins++;
+			$("#wins").text("Wins: " + wins);
+		} else if (score > computerNumber) {
+			losses++;
+			$("#losses").text("Losses: " + losses);
+		} 
+	
+	}
+})
 
 // Play continues until player reaches computer number
    // if player goes over computer number, then player loses and that is added to the loss column
